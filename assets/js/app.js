ac96d17f60a3d04037422a93e38019ee6dc2c479
Nihad Babayev <nihad0babayev@gmail.com>
2022-04-07 18:56:20 +0400

    1 last things only remaining

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,17 +1,67 @@
 const apiKey="42e1ddedf9d266f82ec9787d8ebd12f4";
 const button=document.getElementById('button');
-
+const degree=document.getElementById('degree');
+const area=document.getElementById('Country');
+const Wt=document.getElementById('WT');
+const humid=document.getElementById('humid');
+const wind=document.getElementById('wind');
+const weather=document.getElementById('weather');
+const center=document.getElementById('center');
+const Main=document.getElementById('Main').style;
+const colors={
+    Clear: '#42C2FF',
+    Clouds:'#A8AAC4',
+    Snow:'#6BA7CC'
+}
+let Country='';
+let lon;
+let lat;
+function getWeather(lat,lon){
+    const xhttp=new XMLHttpRequest();
+    xhttp.onload= function Name(){
+    let data=JSON.parse(this.response);
+    Country= Country+', ' + data.sys.country 
+    area.innerText=Country;
+    Country= ''
+    degree.innerText=data.main.temp +'°C'
+    Wt.innerText=data.weather[0].main
+    humid.innerText='Humidity:'+ data.main.humidity+'%'
+    wind.innerText="Wind Speed:"+data.wind.speed +'km/h'
+    let test=`colors.${data.weather[0].main}`
+    
+    weather.src='assets/images/'+ data.weather[0].main +'.png'
+    center.style.backgroundColor=eval(test)
+    Main.setProperty('--background',` url(../Icons/${data.weather[0].main}.svg)`)
+    }
+    xhttp.open("Get",`https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&units=metric&appid=${apiKey}`,true)
+    xhttp.send()
+}
 function getCoords() {
     const xhttp=new XMLHttpRequest();
+    
     const input=document.getElementById('cityName').value;
-    xhttp.onload= function (){
-        let myData= JSON.parse(this.response);
-        console.log(myData);
-    }
+    Country=Country+input;
     xhttp.open("Get", `http://api.openweathermap.org/geo/1.0/direct?q=${input}&limit=5&appid=${apiKey}`, true);
+    xhttp.onreadystatechange= function (){
+        if(xhttp.readyState===4){
+            if (xhttp.status===200) {
+                xhttp.onload=  function  starter (){
+                    let myData= JSON.parse(this.response);
+                    if (myData.length <=0 ) {
+                        alert('Wrong City or another issue please contact with the developer')
+                    }
+                    lon=myData[0].lon
+                    lat=myData[0].lat
+                    getWeather(lat,lon)
+                }
+            }else {
+                alert("Error",xhttp.statusText);
+            }
+        }
+    }
+   
     xhttp.send();
-
-
+ 
 
 
 } 
